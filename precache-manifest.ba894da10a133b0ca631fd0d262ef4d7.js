097e9e5eaead8188d5e96862e96bed9b2f33b699
ajinkya sawant <sawant.ajinkya46@gmail.com>
2020-06-29 14:07:24 +0530

    Test Two commit

---
+++ precache-manifest.ba894da10a133b0ca631fd0d262ef4d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710dc2a64c45c4af44168d6090b8cad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033ede43000c60cc3708",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "019a30e5871bb6201840",
+    "url": "/static/js/2.e4d06f6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4d06f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033ede43000c60cc3708",
+    "url": "/static/js/main.bb6d813c.chunk.js"
+  },
+  {
+    "revision": "8cedc8e8064a91556169",
+    "url": "/static/js/runtime-main.36799215.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
